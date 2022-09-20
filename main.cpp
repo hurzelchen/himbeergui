e762eab3ce25186cafca7490e3877761b7c7d4fb
Hurzelchen <hurzelchen@googlemail.com>
2022-09-20 12:27:10 +0200

    Add fullscreen option to MainWindow

--- main.cpp
+++ main.cpp
@@ -1,14 +1,36 @@
 #include <QApplication>
+#include <QCommandLineParser>
 #include <QMainWindow>
+#include <QSizePolicy>
+
+static const int HIMBEERGUI_WIDTH = 1280;
+static const int HIMBEERGUI_HEIGHT = 400;
 
 int main(int argc, char** argv)
 {
     QApplication app(argc, argv);
 
-    QMainWindow mainWindow(nullptr);
-    mainWindow.show();
+    QCommandLineParser commandLineParser;
 
-    int returnValue = app.exec();
+    QCommandLineOption fullscreenOption({"f", "fullscreen"}, QStringLiteral("Show application fullscreen"));
+    commandLineParser.addOption(fullscreenOption);
+
+    commandLineParser.process(app);
+
+    QMainWindow mainWindow(nullptr);
+
+    if (commandLineParser.isSet(fullscreenOption))
+    {
+        mainWindow.showFullScreen();
+    }
+    else
+    {
+        mainWindow.setFixedSize(HIMBEERGUI_WIDTH, HIMBEERGUI_HEIGHT);
+        mainWindow.setSizePolicy(QSizePolicy::Fixed, QSizePolicy::Fixed);
+        mainWindow.show();
+    }
+
+    int returnValue = QApplication::exec();
 
     return returnValue;
 }
